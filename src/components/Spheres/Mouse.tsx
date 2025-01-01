bf79c1afa666dabb00b3a55e1fd1c76538d19c24
Abdulfettah <fettahaud@gmail.com>
2025-01-02 00:06:37 +0300

    edit

--- src/components/Spheres/Mouse.tsx
+++ src/components/Spheres/Mouse.tsx
@@ -1,14 +1,37 @@
 import { useFrame, useThree } from '@react-three/fiber'
 import { BallCollider, RigidBody } from '@react-three/rapier'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 export default function Mouse() {
   const { viewport } = useThree()
   const mouseSphere = useRef(null)
-  useFrame((state) => {
+  const mouse = useRef({ x: 0, y: 0 })
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      mouse.current.x = (event.clientX / window.innerWidth) * 2 - 1
+      mouse.current.y = -(event.clientY / window.innerHeight) * 2 + 1
+    }
+
+    const handleTouchMove = (event) => {
+      const touch = event.touches[0]
+      mouse.current.x = (touch.clientX / window.innerWidth) * 2 - 1
+      mouse.current.y = -(touch.clientY / window.innerHeight) * 2 + 1
+    }
+
+    window.addEventListener('mousemove', handleMouseMove)
+    window.addEventListener('touchmove', handleTouchMove)
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove)
+      window.removeEventListener('touchmove', handleTouchMove)
+    }
+  }, [])
+
+  useFrame(() => {
     mouseSphere.current.setNextKinematicTranslation({
-      x: (state.mouse.x * viewport.width) / 3,
-      y: (state.mouse.y * viewport.height) / 3,
+      x: (mouse.current.x * viewport.width) / 3,
+      y: (mouse.current.y * viewport.height) / 3,
       z: 0,
     })
   })
